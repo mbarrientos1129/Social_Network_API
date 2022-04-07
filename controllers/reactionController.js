b4d318c82a947acd4aadd2eee3da6ac495dfe42a
mbarrientos.webdevelopment@gmail.com <91514897+mbarrientos1129@users.noreply.github.com>
2022-04-06 22:15:12 -0400

    Add a new reaction controller and modified thought controller

---
+++ controllers/reactionController.js
@@ -0,0 +1,41 @@
+const { Thought } = require('../models')
+
+module.exports = {
+    //Post reaction
+    addReaction(req, res) {
+        Reaction.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { new: true, runValidators: true }
+        )
+            // .populate({
+            //     path: 'reactions',
+            //     select: '-__v'
+            // })
+            // .select('-__v')
+            .then((thought) => 
+            !thought
+            ?res.status(404).json({ message: "No thought has been found with this id but a reaction has been created."})
+            :res.json(thought)
+            )
+            .catch(err => res.json(err));
+    },
+    //Delete Reaction
+    deleteReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: req.body } },
+            { new: true }
+        )
+            .then((thought) => {
+                !thought
+                ?res.status(404).json({ message: "Reaction has been deleted but there was no thought found."})
+                :res.json({ message: "Your reaction has been deleted!"})
+                // if (!dbThoughtData) {
+                //     res.status(404).json({ message: 'No thought with this id!' });
+                //     return;
+                // }
+            })
+            .catch(err => res.json(err));
+    }
+};
